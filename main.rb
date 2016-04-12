fcb12e3d4ff6941d1d5aa7795c8e8eda5efd7abd
Maxime <maxime@MacBook-Pro-de-Maxime-5.local>
2016-04-12 22:51:18 +0200

    STEP4: fourth check

--- main.rb
+++ main.rb
@@ -2,7 +2,9 @@
 
 def grandma(input)
     output = nil
-    if input.empty?
+    if input == "GOODBYE!"
+	output = "LEAVING SO SOON?"
+    elsif input.empty?
        output = "WHAT?!"
     elsif input != input.upcase
        output = "SPEAK UP, KID!"
@@ -26,5 +28,13 @@ prompt = "> "
 until bye == 2 do
     print prompt
     answer = gets.chomp
-    puts grandma(answer)
+    if answer == "GOODBYE!"
+	if bye == 0
+	    puts grandma(answer)
+	end
+	bye += 1
+    else
+    	puts grandma(answer)
+    end
 end
+puts "LATER, SKATER!"
